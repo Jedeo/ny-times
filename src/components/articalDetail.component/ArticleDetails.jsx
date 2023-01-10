a4b274a8820ed79c9f25d9cc56c733550fc67705
Jedeo <jedeopastor@gmail.com>
2023-01-10 15:50:14 -0700

    Add conditional randering

--- src/components/articalDetail.component/ArticleDetails.jsx
+++ src/components/articalDetail.component/ArticleDetails.jsx
@@ -3,17 +3,25 @@ import { getArticles } from "../../apICalls/getArticles";
 import { useEffect } from "react";
 import { useState } from "react";
 import "./ArticleDetails.css";
+import load from "../../resources/loading.mp4"
 
 const ArticleDetails = () => {
   const articleTitle = useParams();
   const [article, setArticle] = useState({});
+  const [loading, setLoading] = useState(false)
+
+  const { abstract, title, byline, short_url, multimedia, published_date } = article;
+  const date = new Date(published_date)
+
 
   useEffect(() => {
+    
     const fetchArticle = async () => {
       const articles = await getArticles();
       const clickedArticle = articles.find(
         (article) => article.title === articleTitle.title
       );
+      setLoading(true)
       setArticle(clickedArticle);
     };
     fetchArticle();
@@ -21,23 +29,22 @@ const ArticleDetails = () => {
 
   console.log(article);
 
-  const { abstract, title, byline, short_url, multimedia, published_date } = article;
-  const date = new Date(published_date)
+ 
 
-  return (
-    <div className="article-container">
-      <div className="image-container">
-        <img className="detail-image"src={multimedia?.[0]?.url} alt={multimedia?.[0]?.caption} />
-      </div>
-      <div className="details-container">
-        <h2>{title}</h2>
-        <p>
-          {abstract} <a href={short_url}>read more</a>
-        </p>
-        <p>published: {date.toLocaleDateString()}</p>
-        <h3>{byline}</h3>
-      </div>
-    </div>
+  return (<>{!loading ?  <video autoPlay loop muted> <source src={load} type="video/mp4"/></video> : <div className="article-container">
+  <div className="image-container">
+    <img className="detail-image"src={multimedia?.[0]?.url} alt={multimedia?.[0]?.caption} />
+  </div>
+  <div className="details-container">
+    <h2>{title}</h2>
+    <p>
+      {abstract} <a href={short_url}>read more</a>
+    </p>
+    <p>published: {date.toLocaleDateString()}</p>
+    <h3>{byline}</h3>
+  </div>
+</div>}</>
+   
   );
 };
 
