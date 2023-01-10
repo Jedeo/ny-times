7b0d42f994375b959af7a0ab9192300948f26928
Jedeo <jedeopastor@gmail.com>
2023-01-10 15:49:47 -0700

    Add try catch block

--- src/apICalls/getArticles.js
+++ src/apICalls/getArticles.js
@@ -1,7 +1,13 @@
 export const getArticles = async () => {
-  const res = await fetch(
+  try {
+    const res = await fetch(
     `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`
   );
     const allArticles = await res.json()
   return allArticles.results
+
+  }catch(err){
+    return err
+  }
+  
 };
