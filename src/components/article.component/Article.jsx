30405ef430d5b5184f57a8f017d85e4f54e07ed9
Jedeo <jedeopastor@gmail.com>
2023-01-10 17:09:07 -0700

    Refactored: style layout

--- src/components/article.component/Article.jsx
+++ src/components/article.component/Article.jsx
@@ -1,12 +1,12 @@
 import "./Article.css";
-import { useNavigate } from "react-router-dom";
 const Article = ({ title, author, img, alt }) => {
 
   return (
       <figure>
+        <h4 className="article-author">{author}</h4>
         <img src={img} alt={alt} />
         <figcaption><h3 className="article-title">{title}</h3></figcaption>
-        <h4 className="article-author">{author}</h4>
+        
       </figure>
   );
 };
