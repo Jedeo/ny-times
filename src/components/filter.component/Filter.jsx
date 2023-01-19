e1e1fc5ef249a80ff8a03bce0157ce63215ce070
Jedeo <jedeopastor@gmail.com>
2023-01-18 17:40:28 -0700

    The home image has been resized and the dropdown menu has been labeled

--- src/components/filter.component/Filter.jsx
+++ src/components/filter.component/Filter.jsx
@@ -1,21 +1,34 @@
+import "./Filter.css";
 
-import "./Filter.css"
+const Filter = ({ filter, allArticles, setFiltered }) => {
+  const options = filter.map((option) => (
+    <option key={option} value={option}>
+      {" "}
+      {option}
+    </option>
+  ));
 
-const Filter = ({filter, allArticles, setFiltered})  => {
-    const options = filter.map(option=> <option  key={option} value={option}> {option}</option>)
-    
-    const handleFilter = (e)=> {
-        const articles = [...allArticles]
-        let filtered = articles.filter(article => article.section === e.currentTarget.value)
-        setFiltered(filtered) 
-    }
-    
-    return (<form action="">
-        <select className='filter-dropDown' name="" id="" onChange={(e)=> handleFilter(e)}>
-            <option value="All"> All </option>
-            {options}
-        </select>
-    </form>)
-} 
+  const handleFilter = (e) => {
+    const articles = [...allArticles];
+    let filtered = articles.filter(
+      (article) => article.section === e.currentTarget.value
+    );
+    setFiltered(filtered);
+  };
 
-export default Filter 
+  return (
+    <form className="filter-dropDown">
+      <label htmlFor="Filter">section:</label>
+      <select
+        name="Filter"
+        id="Filter"
+        onChange={(e) => handleFilter(e)}
+      >
+        <option value="All"> All </option>
+        {options}
+      </select>
+    </form>
+  );
+};
+
+export default Filter;
--- src/components/navbar.component/Navbar.jsx
+++ src/components/navbar.component/Navbar.jsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom"
-import "./Navbar.css"
-import home from "../../resources/home.svg"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import home from "../../resources/home.svg";
+import logo from "../../resources/Logo.svg";
 const Navbar = () => {
-   return <nav className="nav-container"><Link to="/"> <img className="home-icon"src={home} alt="" /> </Link></nav>
-}
+  return (
+    <nav className="nav-container">
+      {/* <img className="page-logo" src={logo} alt="" /> */}
+      <Link to="/">
+        <img className="home-icon" src={home} alt="" />
+      </Link>
+    </nav>
+  );
+};
 
-
-export default Navbar
+export default Navbar;
